0ee11b193513f63c918eb821a69a439f52a9a7ca
Juan Esteban Grisales <jgrisaleses@cesde.net>
2026-03-29 19:16:35 -0500

    Creamos repositorio para usuario, creando tokens de inicio de sesion

---
+++ src/main/java/com/musacreativa/musacreativa/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.musacreativa.musacreativa.repository;
+
+import com.musacreativa.musacreativa.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
---
+++ src/main/java/com/musacreativa/musacreativa/security/JwtService.java
@@ -0,0 +1,55 @@
+package com.musacreativa.musacreativa.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generarToken(String email) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extraerEmail(String token) {
+        return extraerClaims(token).getSubject();
+    }
+
+    public boolean esTokenValido(String token) {
+        try{
+            Claims claims = extraerClaims(token);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extraerClaims(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+}
